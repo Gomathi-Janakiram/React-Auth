c17f0d95e5c20b26d50f411c617835b27bb34f1e
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-09-09 20:42:48 +0530

    conditon for unlogged user to redirect to login page

---
+++ front-end/src/auth/PrivateRoute.js
@@ -0,0 +1,13 @@
+import {Route,Redirect} from 'react-router-dom'
+
+const PrivateRoute=(props)=>{
+    const user=null;
+
+    if(!user){
+        return <Redirect to='/login'/>
+    }
+
+    return <Route {...props}/>
+}
+
+export default PrivateRoute
