ae9ec31443856f4d7150e7e178969a43a1bc32b7
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-09-09 20:38:10 +0530

    added routes for login and signup page

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserInfoPage } from './pages/UserInfoPage';
+import LogInPage from './pages/LogInPage'
+import SignUpPage from './pages/SignUpPage'
+import PrivateRoute from './auth/PrivateRoute'
 
 export const Routes = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/" exact>
+                <PrivateRoute path="/" exact>
                     <UserInfoPage />
+                </PrivateRoute>
+                <Route exact path='/login'>
+                    <LogInPage/>
+                </Route>
+                <Route exact path='/signup'>
+                    <SignUpPage/>
                 </Route>
             </Switch>
         </Router>
