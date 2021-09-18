df69ae19071ae4e957f5fb29962c6a8d1a2f6f3b
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-09-18 19:17:47 +0530

    added sign up route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,8 +1,14 @@
+import {SignupUser} from '../controllers/userController'
+
 const Routes=(app)=>{
     app.route('/')
         .get((req,res)=>{
             res.send("Hello welcome!!!!")
         })
+
+    // sign-up route
+    app.route('/api/signup')
+        .post(SignupUser)
 
     app.route('*')
         .get((req,res)=>{
