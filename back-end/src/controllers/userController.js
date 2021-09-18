934c9f5fbc9d025132b520ccd3fcde6fe7386f16
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-09-18 19:17:16 +0530

    added controller for registering user

---
+++ back-end/src/controllers/userController.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+import userSchema from '../models/userSchema'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const Users=mongoose.model("Users",userSchema)
+
+export const SignupUser=async (req,res)=>{
+    // destructuring user info
+    const {email,password}=req.body;
+    // check in db whether a user is present or not
+    const isUser= await Users.findOne({email:email})
+    if(isUser){
+        // if present he cannot signup
+        res.status(409).json({error:`User ${email} already exists`})
+    }
+    else{
+        // else, hash the password of the user
+        const passwordHash=await bcrypt.hash(password,10);
+
+        const startingInfo={
+            favouriteFood:'',
+            hairColor:'',
+            bio:''
+        }
+
+        
+        // create a new entry
+        const newUser=await new Users({
+            email:email,
+            password:passwordHash,
+            Info:startingInfo,
+            isVerified:false
+        })
+        // save the email and password in db
+        await newUser.save((err,user)=>{
+            if(err){
+                console.log(newUser)
+                console.log(err)
+                res.json({err})
+            }else{
+                console.log(newUser)
+                res.status(200).json({success:`${email} Registered successfully`})
+            }
+        })
+
+        const {id}=newUser 
+        console.log('newUser',newUser)
+        console.log('id',id)
+    }
+}
