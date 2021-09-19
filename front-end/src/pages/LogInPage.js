cc2e9d9e8df62546ae2d915e0a4a6a6e76f4ce5b
Gomathi-07 <gomathi.janakiram@banfico.com>
2021-09-19 21:35:45 +0530

    request to server and add token in localstorage

--- front-end/src/pages/LogInPage.js
+++ front-end/src/pages/LogInPage.js
@@ -1,5 +1,7 @@
 import {React,useState} from 'react'
 import {useHistory} from 'react-router-dom'
+import axios from 'axios'
+import {useToken} from '../auth/useToken'
 
 const LogInPage=()=>{
 
@@ -9,8 +11,17 @@ const LogInPage=()=>{
     const [passwordValue,setPasswordValue]=useState('')
     const [errorMessage,setErrorMessage]=useState('')
 
-    const LoginClicked=()=>{
-        alert('Login functionality not implemented')
+    const [token,setToken]=useToken()
+
+    const handleLogin=async ()=>{
+        const response=await axios.post('http://localhost:8080/api/login',{
+            email:emailValue,
+            password:passwordValue
+        })
+        const {token}=response.data
+        setToken(token)
+        history.push('/')
+        console.log(response)
     }
 
     return(
@@ -20,7 +31,7 @@ const LogInPage=()=>{
             <input placeholder='someone@gmail.com' onChange={e=>setEmailValue(e.target.value)}/>
             <input type='password' placeholder='password' onChange={e=>setPasswordValue(e.target.value)}/>
             <hr/>
-            <button disabled={!emailValue || !passwordValue} onClick={LoginClicked}>Log In</button>
+            <button disabled={!emailValue || !passwordValue} onClick={handleLogin}>Log In</button>
             <button onClick={()=>history.push('/forgot-password')}>Forgot Password?</button>
             <button onClick={()=>history.push('/signup')}>Don't have an account? Sign up</button>
         </div>
